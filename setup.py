3bf8f9bc33849cd7ef719d4b11b64f56ef4505e8
Vivek Jayaram <vivekjayaram@Viveks-MacBook-Pro.local>
2018-04-30 23:03:56 +0200

    Added setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+setup(
+  name = 'pychorus',
+  packages = ['pychorus'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Library to find the choruses and interesting segments of songs',
+  author = 'Vivek Jayaram',
+  author_email = 'vivekjayaram@gmail.com',
+  url = 'https://github.com/vivjay30/pychorus', # use the URL to the github repo
+  download_url = 'https://github.com/vivjay30/pychorus/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['Chorus', 'Audio Signal Processing', 'Music'], # arbitrary keywords
+  install_requires=[
+    'librosa',
+    'numpy',
+    'scipy',
+    'soundfile',
+    'matplotlib'
+  ]
+  classifiers = [],
+)
